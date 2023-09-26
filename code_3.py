63b6b1d631cad83d36590acf414d45a3525b503a
DanyPankrat <140436906+DanyPankrat@users.noreply.github.com>
2023-09-26 21:16:34 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+def generate_random_data():
+import random
+    data = generate_random_data()
+    for item in data:
+
+def main():
+
+
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+        print(f"Random Number: {item}")
